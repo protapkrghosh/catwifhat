0873aa7f12bbe4268dc51ec22793c5da45b74373
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-04-28 16:42:17 +0600

    aa

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,10 @@
+import BannerText from '@/components/sections/BannerText/BannerText';
 import React from 'react';
 
 const Home = () => {
   return (
     <div>
-      <h2>This is home contents</h2>
+      <BannerText />
     </div>
   );
 };
