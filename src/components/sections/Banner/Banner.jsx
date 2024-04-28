428f975cd97a0c2022197c926db10944ae5b79e4
Protap Ghosh <protapg518@gmail.com>
2024-04-28 23:41:48 +0600

    added banner section

--- src/components/sections/Banner/Banner.jsx
+++ src/components/sections/Banner/Banner.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import bannerBg from "@/assets/Banner/Group8.png";
+import catwifhatText from "@/assets/Banner/Group81.png";
+import welcomeIcon from "@/assets/Banner/image419.png";
+import Container from '../Container/Container';
+import { Button } from "@/components/ui/button";
 
 const Banner = () => {
   return (
-    <div>
-      
+    <div className="mb-[116px]">
+      <img src={bannerBg} alt="Image" className=""/>
+      <div className="-mt-[600px] 2xl:-mt-[810px]">
+        <Container>
+          <img src={catwifhatText} alt="Image" className="mx-auto"/>
+
+          <img src={welcomeIcon} alt="Image" className="w-[250px] mt-20" />
+          
+          <div className="flex justify-end -mt-10 -mr-16">
+            <div className="xl:w-[33%] 2xl:w-[29%]">
+              <p className="text-[#272727] text-[18px]">CatwifHat is simply a cat wif a hat, Let's add hats to the cat. CatwifHat Is DogwifHat's Biggest Competitor! !</p>
+
+              <Button className="bg-[#DF355F] text-[18px] navButton px-10 mt-12">Join Now</Button>
+            </div>
+          </div>
+        </Container>
+      </div>
     </div>
   );
 };
