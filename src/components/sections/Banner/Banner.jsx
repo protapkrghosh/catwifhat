5efdfed729164962034da1676f658a8bbed8d512
Protap Ghosh <protapg518@gmail.com>
2024-04-29 00:54:26 +0600

    s

--- src/components/sections/Banner/Banner.jsx
+++ src/components/sections/Banner/Banner.jsx
@@ -7,17 +7,17 @@ import { Button } from "@/components/ui/button";
 const Banner = () => {
   return (
     <div className="mb-[116px]">
-      <img src={bannerBg} alt="Image" className="h-auto 2xl:h-screen w-full" />
+      <img src={bannerBg} alt="Image" className="h-[500px] lg:h-[550px] xl:h-auto 2xl:h-screen w-full" />
       
-      <div className="-mt-[600px] 2xl:-mt-[810px]">
+      <div className="-mt-[430px] lg:-mt-[465px] xl:-mt-[600px] 2xl:-mt-[810px]">
         <Container>
-          <img src={catwifhatText} alt="Image" className="mx-auto w-[760px] 2xl:w-auto"/>
+          <img src={catwifhatText} alt="Image" className="mx-auto w-[500px] xl:w-[760px] 2xl:w-auto"/>
 
-          <img src={welcomeIcon} alt="Image" className="w-[250px] mt-20" />
+          <img src={welcomeIcon} alt="Image" className="w-[130px] lg:w-[160px] xl:w-[160px] 2xl:w-[250px] mt-10 2xl:mt-20" />
           
-          <div className="flex justify-end -mt-10 -mr-16">
-            <div className="xl:w-[33%] 2xl:w-[29%]">
-              <p className="text-[#272727] text-[18px]">CatwifHat is simply a cat wif a hat, Let's add hats to the cat. CatwifHat Is DogwifHat's Biggest Competitor! !</p>
+          <div className="flex justify-end -mt-20 xl:-mt-10 -mr-16">
+            <div className="w-[35%] xl:w-[33%] 2xl:w-[29%]">
+              <p className="text-[#272727] text-[13px] lg:text-[15px] xl:text-[18px]">CatwifHat is simply a cat wif a hat, Let's add hats to the cat. CatwifHat Is DogwifHat's Biggest Competitor! !</p>
 
               <Button className="bg-[#DF355F] hover:bg-[#DF355F] text-[18px] navButton px-10 mt-12">Join Now</Button>
             </div>
