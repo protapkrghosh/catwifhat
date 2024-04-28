630219a680eaeedd33aebd4212c5ef8e14a9d2a5
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-04-29 00:22:17 +0600

    aa

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import Banner from '@/components/sections/Banner/Banner';
 import BannerText from '@/components/sections/BannerText/BannerText';
 import OurPartner from '@/components/sections/OurPartner/OurPartner';
+import Phase from '@/components/sections/Phase/Phase';
 import TryAnotherCat from '@/components/sections/TryAnotherCat/TryAnotherCat';
 import WhyChooseCatwifhat from '@/components/sections/WhyChooseCatwifhat/WhyChooseCatwifhat';
 import React from 'react';
@@ -11,6 +12,7 @@ const Home = () => {
       <Banner />
       <BannerText />
       <WhyChooseCatwifhat />
+      <Phase />
       <OurPartner />
       <TryAnotherCat />
     </div>
