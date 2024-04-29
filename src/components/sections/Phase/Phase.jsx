d427034433b7bf81668cd88db94b45faa99b3cb3
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-04-29 13:09:16 +0600

    aa

--- src/components/sections/Phase/Phase.jsx
+++ src/components/sections/Phase/Phase.jsx
@@ -10,40 +10,23 @@ const Phase = () => {
       <Container>
         <p className='text-[#DF355F] text-center text-[90px] leading-[108px] tracking-[2.7px]'>phase</p>
 
-        <div className='lg:mt-[290px] relative'>
+        <div className='lg:mt-[290px]'>
           <img className='lg:w-auto mx-auto' src={phaseImage} alt="image" />
 
-          {/* first cart */}
-          <div className='absolute 2xl:top-[-226px] 2xl:left-[698px]'>
-            <img className='lg:w-[550px]' src={imageCard1} alt="image" />
-            <div className='absolute 2xl:top-[196px] 2xl:left-[98px]'>
-              <p className='text-[#DF355F] text-[18px] leading-[27px] tracking-[0.72px] lg:mb-[10px]'>stage1</p>
-              <div className='flex items-center'>
-                <div className='bg-[#DF355F] w-[13px] h-[13px] rounded-full'></div>
-                <p className='text-[#3F3F3F] text-[16px] leading-[24px] tracking-[0.64px] lg:mb-[15px]'>Launching CatWifHat Website & Community</p>
-              </div>
-              <div className='flex items-center'>
-                <div className='bg-[#DF355F] w-[13px] h-[13px] rounded-full'></div>
-                <p className='text-[#3F3F3F] text-[16px] leading-[24px] tracking-[0.64px] lg:mb-[15px]'>Launching CatWifHat Website & Community</p>
-              </div>
-              <div className='flex items-center'>
-                <div className='bg-[#DF355F] w-[13px] h-[13px] rounded-full'></div>
-                <p className='text-[#3F3F3F] text-[16px] leading-[24px] tracking-[0.64px] lg:mb-[15px]'>Launching CatWifHat Website & Community</p>
-              </div>
-              <div className='flex items-center'>
-                <div className='bg-[#DF355F] w-[13px] h-[13px] rounded-full'></div>
-                <p className='text-[#3F3F3F] text-[16px] leading-[24px] tracking-[0.64px]'>Launching CatWifHat Website & Community</p>
-              </div>
-            </div>
+          {/*-------------------- first cart -----------------------*/}
+          <div className=''>
+            <img className='lg:w-auto' src={imageCard1} alt="image" />
+
+
           </div>
 
           {/* second cart */}
-          <div className='absolute 2xl:top-[76px] 2xl:left-[140px]'>
+          <div className=''>
             <img className='lg:w-auto' src={imageCard2} alt="image" />
           </div>
 
           {/* third cart */}
-          <div className='absolute 2xl:top-[356px] 2xl:left-[698px]'>
+          <div className=''>
             <img className='lg:w-auto' src={imageCard3} alt="image" />
           </div>
 
