3ef050932124336861b69f3d55b75e90de491947
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-04-29 17:19:16 +0600

    aa

--- src/components/sections/Phase/Phase.jsx
+++ src/components/sections/Phase/Phase.jsx
@@ -15,19 +15,39 @@ const Phase = () => {
 
           {/*-------------------- first cart -----------------------*/}
           <div className=''>
-            <img className='lg:w-auto' src={imageCard1} alt="image" />
+            <img className='lg:w-auto xl:mt-[-875px] xl:ml-[700px]' src={imageCard1} alt="image" />
 
+            {/* title section  */}
+            <div className='xl:mt-[-205px] xl:ml-[774px]'>
+              <p className='text-[#DF355F] text-[18px] leading-[27px] tracking-[0.72px] lg:mb-[10px]'>stage1</p>
+              <div className='flex items-center gap-[10px] lg:mb-[15px]'>
+                <div className='w-[13px] h-[13px] bg-[#DF355F] rounded-full'></div>
+                <p className='text-[#3F3F3F] text-[16px] leading-[24px] tracking-[0.64px]'>Launching CatWifHat Website & Community</p>
+              </div>
+              <div className='flex items-center gap-[10px] lg:mb-[15px]'>
+                <div className='w-[13px] h-[13px] bg-[#DF355F] rounded-full'></div>
+                <p className='text-[#3F3F3F] text-[16px] leading-[24px] tracking-[0.64px]'>Presale On Pinksale</p>
+              </div>
+              <div className='flex items-center gap-[10px] lg:mb-[15px]'>
+                <div className='w-[13px] h-[13px] bg-[#DF355F] rounded-full'></div>
+                <p className='text-[#3F3F3F] text-[16px] leading-[24px] tracking-[0.64px]'>Launch On Raydium...</p>
+              </div>
+              <div className='flex items-center gap-[10px]'>
+                <div className='w-[13px] h-[13px] bg-[#DF355F] rounded-full'></div>
+                <p className='text-[#3F3F3F] text-[16px] leading-[24px] tracking-[0.64px]'>Launch Marketing</p>
+              </div>
+            </div>
 
           </div>
 
           {/* second cart */}
           <div className=''>
-            <img className='lg:w-auto' src={imageCard2} alt="image" />
+            <img className='lg:w-auto xl:mt-[-60px] xl:ml-[135px]' src={imageCard2} alt="image" />
           </div>
 
           {/* third cart */}
           <div className=''>
-            <img className='lg:w-auto' src={imageCard3} alt="image" />
+            <img className='lg:w-auto xl:mt-[-75px] xl:ml-[700px]' src={imageCard3} alt="image" />
           </div>
 
         </div>
