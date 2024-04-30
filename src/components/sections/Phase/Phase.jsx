59abfa092642d63d657fede3a01500eea680a669
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-04-30 18:06:00 +0600

    aa

--- src/components/sections/Phase/Phase.jsx
+++ src/components/sections/Phase/Phase.jsx
@@ -26,20 +26,19 @@ const Phase = () => {
               <img className='lg:mx-0 mx-auto xl:w-auto lg:w-[48.5%] md:w-[65%] 2xl:mt-[-875px] xl:mt-[-875px] lg:mt-[-860px] 2xl:ml-[700px] xl:ml-[622px] lg:ml-[480px]' src={imageCard1} alt="image" />
 
               {/* title section  */}
-              <div className='2xl:mt-[-205px] xl:mt-[-205px] lg:mt-[-205px] md:mt-[-175px] 2xl:ml-[774px] xl:ml-[697px] lg:ml-[540px] md:ml-[180px]'>
+              <div className='2xl:mt-[-205px] xl:mt-[-205px] lg:mt-[-205px] md:mt-[-202px] 2xl:ml-[774px] xl:ml-[697px] lg:ml-[540px] md:ml-[180px]'>
 
-                <p className='textStrokeNav text-[#DF355F] text-[18px] leading-[27px] tracking-[0.72px] lg:mb-[-27px]'>stage1</p>
-                <p className='text-[#DF355F] text-[18px] leading-[27px] tracking-[0.72px] lg:mb-[10px]'>stage1</p>
+                <p className='text-[#DF355F] text-[18px] leading-[27px] tracking-[0.72px] md:mb-[10px]'>stage1</p>
 
-                <div className='flex items-center gap-[10px] lg:mb-[15px]'>
+                <div className='flex items-center gap-[10px] md:mb-[15px]'>
                   <div className='w-[13px] h-[13px] bg-[#DF355F] rounded-full'></div>
                   <p className='text-[#3F3F3F] text-[16px] leading-[24px] tracking-[0.64px]'>Launching CatWifHat Website & Community</p>
                 </div>
-                <div className='flex items-center gap-[10px] lg:mb-[15px]'>
+                <div className='flex items-center gap-[10px] md:mb-[15px]'>
                   <div className='w-[13px] h-[13px] bg-[#DF355F] rounded-full'></div>
                   <p className='text-[#3F3F3F] text-[16px] leading-[24px] tracking-[0.64px]'>Presale On Pinksale</p>
                 </div>
-                <div className='flex items-center gap-[10px] lg:mb-[15px]'>
+                <div className='flex items-center gap-[10px] md:mb-[15px]'>
                   <div className='w-[13px] h-[13px] bg-[#DF355F] rounded-full'></div>
                   <p className='text-[#3F3F3F] text-[16px] leading-[24px] tracking-[0.64px]'>Launch On Raydium...</p>
                 </div>
@@ -56,16 +55,15 @@ const Phase = () => {
               <img className='lg:mx-0 mx-auto xl:w-auto lg:w-[48.5%] md:w-[65%] 2xl:mt-[-35px] xl:mt-[-35px] lg:mt-[-5px] 2xl:ml-[135px] xl:ml-[65px] lg:ml-[-25px]' src={imageCard2} alt="image" />
 
               {/* title section  */}
-              <div className='2xl:mt-[-170px] xl:mt-[-170px] lg:mt-[-165px] md:mt-[-150px] 2xl:ml-[174px] xl:ml-[105px] lg:ml-[1px] md:ml-[180px]'>
+              <div className='2xl:mt-[-170px] xl:mt-[-170px] lg:mt-[-165px] md:mt-[-167px] 2xl:ml-[174px] xl:ml-[105px] lg:ml-[1px] md:ml-[180px]'>
 
-                <p className='textStrokeNav text-[#DF355F] text-[18px] leading-[27px] tracking-[0.72px] lg:mb-[-27px]'>stage2</p>
-                <p className='text-[#DF355F] text-[18px] leading-[27px] tracking-[0.72px] lg:mb-[10px]'>stage2</p>
+                <p className='text-[#DF355F] text-[18px] leading-[27px] tracking-[0.72px] md:mb-[10px]'>stage2</p>
 
-                <div className='flex items-center gap-[10px] lg:mb-[15px]'>
+                <div className='flex items-center gap-[10px] md:mb-[15px]'>
                   <div className='w-[13px] h-[13px] bg-[#DF355F] rounded-full'></div>
                   <p className='text-[#3F3F3F] text-[16px] leading-[24px] tracking-[0.64px]'>Listing CMC & CGK</p>
                 </div>
-                <div className='flex items-center gap-[10px] lg:mb-[15px]'>
+                <div className='flex items-center gap-[10px] md:mb-[15px]'>
                   <div className='w-[13px] h-[13px] bg-[#DF355F] rounded-full'></div>
                   <p className='text-[#3F3F3F] text-[16px] leading-[24px] tracking-[0.64px]'>#CatWifHat Trending Global</p>
                 </div>
@@ -82,16 +80,15 @@ const Phase = () => {
               <img className='lg:mx-0 mx-auto xl:w-auto lg:w-[48.5%] md:w-[65%] 2xl:mt-[-45px] xl:mt-[-45px] lg:mt-[-22px] 2xl:ml-[700px] xl:ml-[622px] lg:ml-[480px]' src={imageCard3} alt="image" />
 
               {/* title section  */}
-              <div className='2xl:mt-[-172px] xl:mt-[-172px] lg:mt-[-165px] md:mt-[-150px] 2xl:ml-[774px] xl:ml-[697px] lg:ml-[540px] md:ml-[180px]'>
+              <div className='2xl:mt-[-172px] xl:mt-[-172px] lg:mt-[-165px] md:mt-[-167px] 2xl:ml-[774px] xl:ml-[697px] lg:ml-[540px] md:ml-[180px]'>
 
-                <p className='textStrokeNav text-[#DF355F] text-[18px] leading-[27px] tracking-[0.72px] lg:mb-[-27px]'>stage3</p>
-                <p className='text-[#DF355F] text-[18px] leading-[27px] tracking-[0.72px] lg:mb-[10px]'>stage3</p>
+                <p className='text-[#DF355F] text-[18px] leading-[27px] tracking-[0.72px] md:mb-[10px]'>stage3</p>
 
-                <div className='flex items-center gap-[10px] lg:mb-[15px]'>
+                <div className='flex items-center gap-[10px] md:mb-[15px]'>
                   <div className='w-[13px] h-[13px] bg-[#DF355F] rounded-full'></div>
                   <p className='text-[#3F3F3F] text-[16px] leading-[24px] tracking-[0.64px]'>Listing CMC & CGK</p>
                 </div>
-                <div className='flex items-center gap-[10px] lg:mb-[15px]'>
+                <div className='flex items-center gap-[10px] md:mb-[15px]'>
                   <div className='w-[13px] h-[13px] bg-[#DF355F] rounded-full'></div>
                   <p className='text-[#3F3F3F] text-[16px] leading-[24px] tracking-[0.64px]'>#CatWifHat Trending Global</p>
                 </div>
