f85023ae219bfa5d1a906fb332b9ff70e8a1188e
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-04-30 17:50:06 +0600

    aa

--- src/components/sections/Phase/Phase.jsx
+++ src/components/sections/Phase/Phase.jsx
@@ -23,7 +23,7 @@ const Phase = () => {
 
             {/*-------------------- first cart -----------------------*/}
             <div className=''>
-              <img className='lg:mx-0 mx-auto lg:w-auto md:w-[65%] 2xl:mt-[-875px] xl:mt-[-875px] lg:mt-[-875px] 2xl:ml-[700px] xl:ml-[622px] lg:ml-[480px]' src={imageCard1} alt="image" />
+              <img className='lg:mx-0 mx-auto xl:w-auto lg:w-[48.5%] md:w-[65%] 2xl:mt-[-875px] xl:mt-[-875px] lg:mt-[-860px] 2xl:ml-[700px] xl:ml-[622px] lg:ml-[480px]' src={imageCard1} alt="image" />
 
               {/* title section  */}
               <div className='2xl:mt-[-205px] xl:mt-[-205px] lg:mt-[-205px] md:mt-[-175px] 2xl:ml-[774px] xl:ml-[697px] lg:ml-[540px] md:ml-[180px]'>
@@ -53,10 +53,10 @@ const Phase = () => {
 
             {/* --------------------second cart------------------- */}
             <div className='lg:mt-0 md:mt-[90px]'>
-              <img className='lg:mx-0 mx-auto lg:w-auto md:w-[65%] 2xl:mt-[-35px] xl:mt-[-35px] lg:mt-[-35px] 2xl:ml-[135px] xl:ml-[65px] lg:ml-[-50px]' src={imageCard2} alt="image" />
+              <img className='lg:mx-0 mx-auto xl:w-auto lg:w-[48.5%] md:w-[65%] 2xl:mt-[-35px] xl:mt-[-35px] lg:mt-[-5px] 2xl:ml-[135px] xl:ml-[65px] lg:ml-[-25px]' src={imageCard2} alt="image" />
 
               {/* title section  */}
-              <div className='2xl:mt-[-170px] xl:mt-[-170px] lg:mt-[-170px] md:mt-[-150px] 2xl:ml-[174px] xl:ml-[105px] lg:ml-[-25px] md:ml-[180px]'>
+              <div className='2xl:mt-[-170px] xl:mt-[-170px] lg:mt-[-165px] md:mt-[-150px] 2xl:ml-[174px] xl:ml-[105px] lg:ml-[1px] md:ml-[180px]'>
 
                 <p className='textStrokeNav text-[#DF355F] text-[18px] leading-[27px] tracking-[0.72px] lg:mb-[-27px]'>stage2</p>
                 <p className='text-[#DF355F] text-[18px] leading-[27px] tracking-[0.72px] lg:mb-[10px]'>stage2</p>
@@ -79,10 +79,10 @@ const Phase = () => {
 
             {/* ------------------------third cart---------------------- */}
             <div className='lg:mt-0 md:mt-[110px]'>
-              <img className='lg:mx-0 mx-auto lg:w-auto md:w-[65%] 2xl:mt-[-45px] xl:mt-[-45px] lg:mt-[-45px] 2xl:ml-[700px] xl:ml-[622px] lg:ml-[480px]' src={imageCard3} alt="image" />
+              <img className='lg:mx-0 mx-auto xl:w-auto lg:w-[48.5%] md:w-[65%] 2xl:mt-[-45px] xl:mt-[-45px] lg:mt-[-22px] 2xl:ml-[700px] xl:ml-[622px] lg:ml-[480px]' src={imageCard3} alt="image" />
 
               {/* title section  */}
-              <div className='2xl:mt-[-172px] xl:mt-[-172px] lg:mt-[-172px] md:mt-[-150px] 2xl:ml-[774px] xl:ml-[697px] lg:ml-[540px] md:ml-[180px]'>
+              <div className='2xl:mt-[-172px] xl:mt-[-172px] lg:mt-[-165px] md:mt-[-150px] 2xl:ml-[774px] xl:ml-[697px] lg:ml-[540px] md:ml-[180px]'>
 
                 <p className='textStrokeNav text-[#DF355F] text-[18px] leading-[27px] tracking-[0.72px] lg:mb-[-27px]'>stage3</p>
                 <p className='text-[#DF355F] text-[18px] leading-[27px] tracking-[0.72px] lg:mb-[10px]'>stage3</p>
